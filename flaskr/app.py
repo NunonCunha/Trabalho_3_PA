af4e2d119082a0fe55cde89e5103322ea65a38cf
NunonCunha <nunoncunha@gmail.com>
2022-01-09 01:03:53 +0000

    código final

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,14 +1,105 @@
-import codecs
+import random
+import json
 from flask import Flask, render_template, request
+from flask.wrappers import Request
 
-# Variáveis
+
+'''Variáveis'''
+
+#Variavel para o random
+indice = 0
+#recebe todas as perguntas vindas do ficheiro json
+lista_perguntas = []
+#recebe todas as respostas vindas do ficheiro json
+lista_respostas = []
+#lista para validar os números random gerados, de forma a não haver repetições
+lista_indice = []
+#Lista de perguntas randomizadas
+perguntasRandom = []
+#Lista de respostas randomizadas
+respostasRandom = []
+#Lista temporária para validar as respostas submetidas
+respostasTemp = []
+#contador para as respostas certas
+respostasCertas = 0
+#Dicionário para receber as respostas do utilizador
 respostas = {}
+falhadas = 5*[None]
 
 
-#Funções
+
+'''Funções'''
 
 # Tratamento do Ficheiro
+def trataFicheiro():
+    try:
+        file = open('perguntas.json', encoding='utf-8') 
+        perguntas = json.load(file)
+        print("Ficheiro aberto com sucesso...")
+    except:
+        print("Não foi possivel abrir o ficheiro")
 
+    #passa as perguntas do ficheiro json para uma lista de perguntas
+    for pergunta in perguntas.keys():
+        lista_perguntas.append(pergunta)
+    #passa as respostas do ficheiro json para uma lista de perguntas
+    for resposta in perguntas.values():        
+        lista_respostas.append(resposta)
+
+    file.close()
+
+#Random Perguntas
+def randomPR():
+    
+    trataFicheiro()
+    '''
+    for i in range(5):
+        #Random do indice das perguntas com o range do tamanho do ficheiro
+        indice = random.randint(0, len(lista_perguntas)-1)
+        if indice in lista_indice:
+            #caso o indice já exista na lista, é randomizado um número até não se repetir
+            indice = random.randint(0, len(lista_perguntas)-1)
+            perguntasRandom.append(lista_perguntas[indice])
+            respostasRandom.append(lista_respostas[indice])
+        else:
+            lista_indice.append(indice)
+            perguntasRandom.append(lista_perguntas[indice])
+            respostasRandom.append(lista_respostas[indice])
+    '''
+
+    while len(lista_indice) < 5:
+        indice = random.randint(0, len(lista_perguntas)-1)
+        if indice in lista_indice:
+            #caso o indice já exista na lista, é randomizado um número até não se repetir
+            indice = random.randint(0, len(lista_perguntas)-1)
+            # perguntasRandom.append(lista_perguntas[indice])
+            # respostasRandom.append(lista_respostas[indice])
+        else:
+            lista_indice.append(indice)
+            perguntasRandom.append(lista_perguntas[indice])
+            respostasRandom.append(lista_respostas[indice])
+
+
+   
+
+#Tratamento de respostas
+def tratamentoRespostas(res):
+    
+    respostasCertas = 0
+    
+    for resposta in res.values():        
+        respostasTemp.append(resposta)
+
+    for i in range(5):
+        if respostasRandom[i] == respostasTemp[i]:
+            respostasCertas += 1
+            falhadas[i] = ""
+        else:
+            falhadas[i] = perguntasRandom[i]
+
+    return respostasCertas
+
+    
 
 #Criação da aplicação Flask
 app = Flask(__name__)
@@ -16,17 +107,18 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def get():
 
-        return render_template("quiz.html", Pergunta1="esta é uma pergunta vinda do python")
-
-
+    randomPR()
+    return render_template("quiz.html", Pergunta1 = perguntasRandom[0], Pergunta2 = perguntasRandom[1], Pergunta3 = perguntasRandom[2], Pergunta4 = perguntasRandom[3], Pergunta5 = perguntasRandom[4])
+    
 
 @app.route("/", methods=['post'])
 def post():
 
     respostas = request.form
-  
-    return respostas
-
+    tratamentoRespostas(respostas)
+    result = tratamentoRespostas(respostas)
+ 
+    return render_template("resultado.html", resultado = result, fail1 = falhadas[0], fail2 = falhadas[1], fail3 = falhadas[2], fail4 = falhadas[3], fail5 = falhadas[4] )
 
 if __name__ == "__main__":
 
--- flaskr/tests/testfile.py
+++ flaskr/tests/testfile.py
@@ -2,40 +2,66 @@ import json
 import random
 
 
-
-indice = random.randint(0, 15)
+#Variáveis
+indice = 0
 lista_perguntas = []
 lista_respostas = []
 lista_indice = []
+respostas = { "pergunta1": "verdadeiro", "pergunta2": "falso", "pergunta3": "verdadeiro", "pergunta4": "falso", "pergunta5": "verdadeiro"}
+perguntasRandom = []
+respostasRandom = []
+respostasTemp = []
+respostasCertas = 0
+
  
-try:
-    with open('perguntas.json', encoding='utf-8') as f:
-        perguntas = json.load(f)
+def trataFicheiro():
+    try:
+        file = open('perguntas.json', encoding='utf-8') 
+        perguntas = json.load(file)
         print("Ficheiro aberto com sucesso...")
-except:
-    print("Não foi possivel abrir o ficheiro")
+    except:
+        print("Não foi possivel abrir o ficheiro")
 
 
-for pergunta in perguntas.keys():
+    for pergunta in perguntas.keys():
 
-    lista_perguntas.append(pergunta)
+        lista_perguntas.append(pergunta)
 
-for resposta in perguntas.values():
+    for resposta in perguntas.values():
+        
+        lista_respostas.append(resposta)
+
+    file.close()
+
+def randomPR():
+    trataFicheiro()
+    for x in range(5):
+        indice = random.randint(0, len(lista_perguntas)-1)
+        if indice in lista_indice:
+            indice = random.randint(0, len(lista_perguntas)-1)
+            perguntasRandom.append(lista_perguntas[indice])
+            respostasRandom.append(lista_respostas[indice])
+        else:
+            lista_indice.append(indice)
+
+            perguntasRandom.append(lista_perguntas[indice])
+            respostasRandom.append(lista_respostas[indice])
+
+
+def tratamentoRespostas(res):
+
+    respostasCertas = 0
     
-    lista_respostas.append(resposta)
+    for resposta in res.values():        
+        respostasTemp.append(resposta)
+
+    for i in range(5):
+        if respostasRandom[i] == respostasTemp[i]:
+            respostasCertas += 1
+    return respostasCertas
 
 
-for x in range(5):
-    indice = random.randint(0, 15)
-    if indice in lista_indice:
-        indice = random.randint(0, 15)
-        print(lista_perguntas[indice])
-        print(lista_respostas[indice])
-    else:
-        lista_indice.append(indice)
+randomPR()
+acertou = tratamentoRespostas(respostas)
 
-        print(lista_perguntas[indice])
-        print(lista_respostas[indice])
-
-
-
+print(acertou)
